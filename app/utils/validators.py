cddd828b75da8bf087d2f06ebfab9d219c711fd4
nuvic <78891638+nuvic@users.noreply.github.com>
2022-05-22 08:26:57 -0700

    fix required attribute spelling

--- app/utils/validators.py
+++ app/utils/validators.py
@@ -24,7 +24,7 @@ class AccountValidator(Schema):
         ],
     )
     email = fields.Email(required=True, load_only=True)
-    password = fields.Str(requierd=True, load_only=True)
+    password = fields.Str(required=True, load_only=True)
 
 
 class EmailValidator(Schema):
